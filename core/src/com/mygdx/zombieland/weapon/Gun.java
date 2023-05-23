a78c4b82c69c239ffa5b7a91e6fa8d66f4ca98fa
YinLin <dinhquoclam02@gmail.com>
2023-05-23 13:57:30 +0700

    Add Gun

---
+++ core/src/com/mygdx/zombieland/weapon/Gun.java
@@ -0,0 +1,26 @@
+package com.mygdx.zombieland.weapon;
+
+import com.badlogic.gdx.audio.Sound;
+import com.mygdx.zombieland.World;
+import com.mygdx.zombieland.entity.projectile.Projectile;
+import com.mygdx.zombieland.entity.projectile.ProjectileSource;
+
+public interface Gun extends Weapon {
+
+    float getRecoil();
+
+    void setRecoil(float recoil);
+
+    Projectile launchProjectile(World world, ProjectileSource source);
+
+    int getMaxAmmo();
+
+    Sound getShootingSound();
+
+    Sound getEmptySound();
+
+    Sound getReloadReleaseSound();
+
+    long getReloadDuration();
+
+}
