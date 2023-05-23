a95c7bac0d366e8e6a3b7fea635861b97b6e6b9c
kingstonduy <kingstonduy.duong@gmail.com>
2023-05-23 16:32:29 +0700

    push main

--- core/src/com/mygdx/zombieland/ZombieLandMain.java
+++ core/src/com/mygdx/zombieland/ZombieLandMain.java
@@ -8,16 +8,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ZombieLandMain extends ApplicationAdapter {
-    public static final String gameVersion = "1.0.0";
+    public static final String gameVersion = "1.0.0 beta 1";
+    private static final long STARTUP_BANNER_DURATION = 3000;
 
     private SpriteBatch batch;
+    private World world;
+    private final long startMillis;
+    private Texture phourTeamBannerTexture;
 
     public ZombieLandMain() {
+        this.startMillis = System.currentTimeMillis();
     }
 
     @Override
     public void create() {
         this.batch = new SpriteBatch();
+        this.world = new World(batch);
+        this.world.create();
+
+        this.phourTeamBannerTexture = new Texture(Gdx.files.internal("phour_team.PNG"));
     }
 
     @Override
@@ -25,7 +34,16 @@ public class ZombieLandMain extends ApplicationAdapter {
         ScreenUtils.clear(0.96078431372f, 0.96078431372f, 0.96078431372f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        if (startMillis + STARTUP_BANNER_DURATION > System.currentTimeMillis()) {
+            this.batch.begin();
+            this.getBatch().draw(this.phourTeamBannerTexture, 0, 0);
+            this.batch.end();
+        } else {
 
+            this.batch.begin();
+            this.world.render(); // Render a whole world
+            this.batch.end();
+        }
     }
 
     public SpriteBatch getBatch() {
@@ -35,5 +53,6 @@ public class ZombieLandMain extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
+        this.world.dispose();
     }
 }
